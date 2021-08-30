dc3d71a005856814c99d3c652d76a328c2ac6f0f
7heKnight <60308445+7heKnight@users.noreply.github.com>
2021-08-30 14:58:45 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ for i in lib:
 print('[*] Creating "AES_Cryptor" Folder')
 os.system(f'mkdir AES_Cryptor')
 print('[*] Downloading "AES_Encrypt.py"...')
-os.system(f'curl https://raw.githubusercontent.com/7heKnight/7heknight.py/main/MyTools/AES_Cryptor/AES_Encrypt.py > AES_Cryptor/AES_Encrypt.py')
+os.system(f'curl https://raw.githubusercontent.com/7heKnight/AES_Cryptor/main/AES_Decrypt.py > AES_Cryptor/AES_Encrypt.py')
 print('[*] Downloading "AES_Encrypt.py"...')
-os.system(f'curl https://raw.githubusercontent.com/7heKnight/7heknight.py/main/MyTools/AES_Cryptor/AES_Decrypt.py > AES_Cryptor/AES_Decrypt.py')
+os.system(f'curl https://raw.githubusercontent.com/7heKnight/AES_Cryptor/main/AES_Decrypt.py > AES_Cryptor/AES_Decrypt.py')
